7688330f40a048013ab04bd1c890c6dce674b765
Monica Dimitrova <monicabdimitrova@gmail.com>
2015-09-10 13:05:59 +0300

    Load common elements with jQuery

--- js/main.js
+++ js/main.js
@@ -1,42 +1,41 @@
-
-// Fireup the plugins
 $(document).ready(function(){
-	
-	// initialise  slideshow
-	 $('.flexslider').flexslider({
-        animation: "slide",
-        start: function(slider){
-          $('body').removeClass('loading');
-        }
-      });
 
+  // Include common elements
+  $("#footer").load("common/footer.html");
+  $("#header").load("common/header.html", function()
+  {
+    // Add current navigation class based on url
+    locations = location.pathname.split("/");
+    $('#topnav a[href^="' + locations[locations.length - 1] + '"]').parent().addClass('current');
+
+    /**
+    * Handles toggling the navigation menu for small screens.
+    */
+
+    var button = document.getElementById( 'topnav' ).getElementsByTagName( 'div' )[0],
+        menu   = document.getElementById( 'topnav' ).getElementsByTagName( 'ul' )[0];
+
+    if ( undefined === button )
+      return false;
+
+    if ( undefined === menu || ! menu.childNodes.length )
+    {
+      button.style.display = 'none';
+      return false;
+    }
+
+    button.onclick = function()
+    {
+      if ( -1 == menu.className.indexOf( 'srt-menu' ) )
+        menu.className = 'srt-menu';
+
+      if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
+        button.className = button.className.replace( ' toggled-on', '' );
+        menu.className = menu.className.replace( ' toggled-on', '' );
+      } else {
+        button.className += ' toggled-on';
+        menu.className += ' toggled-on';
+      }
+    };
+  });
 });
-/**
- * Handles toggling the navigation menu for small screens.
- */
-( function() {
-	var button = document.getElementById( 'topnav' ).getElementsByTagName( 'div' )[0],
-	    menu   = document.getElementById( 'topnav' ).getElementsByTagName( 'ul' )[0];
-
-	if ( undefined === button )
-		return false;
-
-	// Hide button if menu is missing or empty.
-	if ( undefined === menu || ! menu.childNodes.length ) {
-		button.style.display = 'none';
-		return false;
-	}
-
-	button.onclick = function() {
-		if ( -1 == menu.className.indexOf( 'srt-menu' ) )
-			menu.className = 'srt-menu';
-
-		if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
-			button.className = button.className.replace( ' toggled-on', '' );
-			menu.className = menu.className.replace( ' toggled-on', '' );
-		} else {
-			button.className += ' toggled-on';
-			menu.className += ' toggled-on';
-		}
-	};
-} )();
